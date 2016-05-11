9bbce14c00864e48658964adf6c3f10e81594a50
jc321013 <jared.marcolongo@my.jcu.edu.au>
2016-05-11 21:57:23 +1000

    started app functions and class

--- App A2.py
+++ App A2.py
@@ -1,4 +1,4 @@
-from kivy.app  import App
+from kivy.app import App
 from kivy.lang import Builder
 
 
@@ -9,43 +9,44 @@ class ExperimentHire(App):
     def build(self):
         self.title = "Experiment Hire"
         self.root = Builder.load_file('GUI A2.kv')
-        return self.root
-
-    def list_items(self):
-        in_file= open("items.csv", 'r', encoding='utf-8')
+        in_file = open("items.csv", 'r', encoding='utf-8')
         items = in_file.readlines()
         in_file.close()
         for line in items:
             section = line.split(',')
             print(section)
-        self.root.ids.rusty_bucket.text = 'item_rustyBucket()'
-        self.root.ids.thermo_mix.text = 'item_thermomix'
-        self.root.ids.golf_cart.text = 'item_golfCart()'
-        self.root.ids.aero_press.text = 'item_AeroPress()'
-        self.root.ids.guitar.text = 'item_guitar()'
+        return self.root
+
+    def list_items(self):
+        self.root.ids.list_items.text = "List Items"
 
     def item_rustyBucket(self):
         name = 'Rusty Bucket(40L bucket-quite rusty), $0.00 is in'
+        self.root.ids.rusty_bucket.text = 'Rusty Bucket'
         self.root.ids.action_label.text = str(name)
         return name
 
     def item_thermomix(self):
         name = 'Thermomix(TM-31), $25.5 is out'
+        self.root.ids.thermo_mix.text = 'Thermomix'
         self.root.ids.action_label.text = str(name)
         return name
 
     def item_golfCart(self):
         name = 'Golf Cart(Tesla powered 250 turbo), $195 is out'
+        self.root.ids.golf_cart.text = 'Golf Cart'
         self.root.ids.action_label.text = str(name)
         return name
 
     def item_AeroPress(self):
         name = 'AeroPress(great coffee maker), $5 is in'
+        self.root.ids.aero_press.text = 'Aero Press'
         self.root.ids.action_label.text = str(name)
         return name
 
     def item_Guitar(self):
         name = 'Guitar(JTV-59), $12.95 is in'
+        self.root.ids.guitar.text = 'Guitar'
         self.root.ids.action_label.text = str(name)
         return name
 
--- assignment.py
+++ assignment.py
@@ -1,38 +1,135 @@
-from kivy.app  import App
-from kivy.lang import Builder
+__JaredMarcolongo__ = 'jc321013'
+
+""" CP1404 Assignment 1 - 2016
+    Items for Hire - a console-based item hiring program
+    Jared Marcolongo
+    21/03/2016
+    https://github.com/jc321013/Cp1404-Assignment-1 """
+
+""" Pseudocode:
+
+function main()
+    display title of document and name
+    display how many items loaded from items.csv
+    open csv file of in stock items
+    close csv file
+    display items
+    display menu
+    get choice
+    while choice is not 'Q'
+        if choice is 'L'
+            display items
+        else if choice is 'H'
+            call hiring_an_item()
+        else if choice is 'R'
+            return item
+        else if choice is 'A'
+            call loading_an_item()
+        else
+            display invalid choice message
+        display menu
+        get choice
+    print farewell message
 
 
-class ExperimentHire(App):
-    def build(self):
-        self.title = "Experiment Hire"
-        self.root = Builder.load_file('assignment.kv')
-        return self.root
-
-    def item_rustyBucket(self):
-        rusty_bucket = str(self.root.ids.rusty_bucket.text)
-        name = 'Rusty Bucket(40L bucket-quite rusty), $0.00 is in'
-        self.root.ids.action_label.text = str(rusty_bucket)
-        return name
+function loading_an_item()
+    get item name, description and price to add
+    open items.csv as fileout for writing
+    for each item in items
+        write item to fileout
+        write newline to fileout
+    close fileout
+    add item to csv file
+        display the new item added
 
 
-    def item_thermomix(self):
-        Thermomix = str(self.root.ids.thermo_mix.text)
-        self.root.ids.action_label.text = str(Thermomix)
+function hiring_an_item()
+    display available item
+    get item choice
+        display item chosen
+    chosen item returns an * for unavailable
+        display available items
+    if no items available
+        display unavailable message
+    display menu    """
+
+import csv
+
+Menu = "\nMenu:\n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new item to stock\n(Q)uit"
 
 
-    def item_golfCart(self):
-        rusty_bucket = str(self.root.ids.golf_cart.text)
-        self.root.ids.action_label.text = str(rusty_bucket)
-
-    def item_AeroPress(self):
-        rusty_bucket = str(self.root.ids.aero_press.text)
-        self.root.ids.action_label.text = str(rusty_bucket)
-
-    def item_Guitar(self):
-        rusty_bucket = str(self.root.ids.guitar.text)
-        self.root.ids.action_label.text = str(rusty_bucket)
+def main():
+    print("Items for Hire- by Jared Marcolongo")
+    print("3 items loaded from items.csv")
 
 
+    # reads from csv file and displays lines in file
+    in_file = open("items.csv", "r", encoding='utf-8')
+    reader = csv.reader(in_file.readlines())
+    in_file.close()
+    for rows in reader:
+        print(rows)
 
-# create and start the App running
-ExperimentHire().run()
+    print(Menu)
+    # .upper allows user to input both lower and upper case letters
+    choice = input("Choice: ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("All items on file(* indicated item is currently out):")
+            print(Items)
+        elif choice == "H":
+            hiring_an_item()
+        elif choice == "R":
+            return_item = input("Number of an item to return: ")
+            print(return_item, "returned")
+        elif choice == "A":
+            loading_items()
+        else:
+            print("Invalid menu choice.")
+        print(Menu)
+        choice = input("Enter your choice: ").upper()
+
+    print("5 items saved to items.csv\n" "Have a nice day")
+
+
+def loading_items():
+        # empty list ofitems, function adds new items to that list
+        add_item = []
+        item = 'item_name', 'item_description', 'item_price'
+        item_name = input("Item name: ")
+        item_description = input("Description: ")
+        item_price = input("Price per day: ")
+        print(item_name, (item_description), "$", item_price)
+        print(item)
+        for item in add_item:
+            # adds item to add_item
+            add_item.append(item)
+            # writes to file items.csv with the added item
+            out_file = open("items.csv", "a")
+            out_file.write(add_item)
+            out_file.close()
+
+
+def hiring_an_item():
+    # function loads items and allows user to choose available stock
+    print("All items on file(* indicates item is currently out)\n", Items)
+    hiring_choice = input("Enter the number of an item to hire: ")
+    while hiring_choice != "":
+        if hiring_choice == "0":
+            print("Rusty Bucket hired for $0.00")
+        elif hiring_choice == "1":
+            print("Golf cart currently out of stock")
+        elif hiring_choice == "2":
+            print("Thermomix currently out of stock")
+        elif hiring_choice == "3":
+            print("AeroPress hired for $5")
+        elif hiring_choice == "4":
+            print("Guitar hired for $12.95")
+    else:
+        print("Invalid item choice")
+    print(Items)
+    hiring_choice = input("Enter the number of an item to hire: ")
+
+
+
+main()
