c4ca1ecd8588e4af63a822ae870a6de597f905fb
jc321013 <jared.marcolongo@my.jcu.edu.au>
2016-05-13 22:30:38 +1000

    pop up started

--- AppA2.py
+++ AppA2.py
@@ -1,16 +1,22 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 from item import Item
 # from Assignment1 import loading_items
 
 
 class ExperimentHire(App, Item):
+    status_text = StringProperty()
+
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(ExperimentHire, self).__init__(**kwargs)
+        self.experimentHire = {}
 
     def build(self):
         self.title = "Experiment Hire"
         self.root = Builder.load_file('GUI A2.kv')
+        self.create_entry_buttons()
         # file = 'items.csv'
         # in_file = open(file, 'r', encoding='utf-8')
         # items = in_file.readlines()
@@ -38,6 +44,41 @@ class ExperimentHire(App, Item):
         self.root.ids.hire_item.text = 'Hire Items'
         self.root.ids.action_label.text = str(label_display)
         return label_display
+
+    def create_entry_buttons(self):
+        for name in self.experimentHire:
+            temp_button = Button(text=name)
+            temp_button.bind(on_release=self.press.entry)
+            # self.root.ids.entriesBox.add_widget(temp_button)
+
+    def press_entry(self, instence):
+        name = instence.text
+        self.status_text = "{}'s number is {}".format(name, self.phonebook[name])
+
+    def press_add(self):
+        self.status_text = "Enter details for new Item"
+        self.root.ids.popup.open()
+
+    def press_save(self, added_name, added_number):
+        self.phonebook[added_name] = added_number
+        self.root.ids.entriesBox.cols = len(self.phonebook) // 5 + 1
+        temp_button = Button(text=added_name)
+        temp_button.bind(on_release=self.press_entry)
+        self.root.ids.entriesBox.add_widget(temp_button)
+        self.root.ids.popup.dismiss()
+        self.clear_fields()
+
+    def clear_fields(self):
+        self.root.ids.addedName.text = ""
+        self.root.ids.addedNumber.text = ""
+
+    def press_cancel(self):
+        self.root.ids.popup.dismiss()
+        self.clear_fields()
+        self.status_text = ""
+
+
+
 
 
 
