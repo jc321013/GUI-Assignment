8af99c37ccedb219bc0a33f27d0e9d404d750c99
jc321013 <jared.marcolongo@my.jcu.edu.au>
2016-05-14 13:19:40 +1000

    pop up started

--- AppA2.py
+++ AppA2.py
@@ -3,11 +3,12 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
 from item import Item
+from itemlist import ItemList
 # from Assignment1 import loading_items
 
 
-class ExperimentHire(App, Item):
-    status_text = StringProperty()
+class ExperimentHire(App, Item, ItemList):
+    action_label = StringProperty()
 
     def __init__(self, **kwargs):
         super(ExperimentHire, self).__init__(**kwargs)
@@ -17,20 +18,9 @@ class ExperimentHire(App, Item):
         self.title = "Experiment Hire"
         self.root = Builder.load_file('GUI A2.kv')
         self.create_entry_buttons()
-        # file = 'items.csv'
-        # in_file = open(file, 'r', encoding='utf-8')
-        # items = in_file.readlines()
-        # in_file.close()
-        # for line in items:
-        #     section = line.split(',')
-        #     print(section)
         return self.root
 
-    def list_items(self):
-        label_display = 'Choose action from the left menu, then select items on the right'
-        self.root.ids.list_items.text = "List Items"
-        self.root.ids.action_label.text = str(label_display)
-        return label_display
+
 
     def hiring_item(self):
         # in_file = open("items.csv", 'r', encoding='utf-8')
@@ -49,33 +39,33 @@ class ExperimentHire(App, Item):
         for name in self.experimentHire:
             temp_button = Button(text=name)
             temp_button.bind(on_release=self.press.entry)
-            # self.root.ids.entriesBox.add_widget(temp_button)
+            self.root.ids.newItem.add_widget(temp_button)
 
-    def press_entry(self, instence):
-        name = instence.text
-        self.status_text = "{}'s number is {}".format(name, self.phonebook[name])
+    def press_entry(self, instance):
+        name = instance.text
+        self.root.ids.action_label.text = "{} $ {} Per Day".format(name, self.experimentHire[name])
 
     def press_add(self):
-        self.status_text = "Enter details for new Item"
+        self.action_label = "Enter details for new Item"
         self.root.ids.popup.open()
 
-    def press_save(self, added_name, added_number):
-        self.phonebook[added_name] = added_number
-        self.root.ids.entriesBox.cols = len(self.phonebook) // 5 + 1
+    def press_save(self, added_name, added_description, added_price):
+        self.experimentHire[added_name] = added_description, added_price
+        self.root.ids.newItem.cols = len(self.experimentHire)
         temp_button = Button(text=added_name)
         temp_button.bind(on_release=self.press_entry)
-        self.root.ids.entriesBox.add_widget(temp_button)
+        self.root.ids.newItem.add_widget(temp_button)
         self.root.ids.popup.dismiss()
         self.clear_fields()
 
     def clear_fields(self):
         self.root.ids.addedName.text = ""
-        self.root.ids.addedNumber.text = ""
+        self.root.ids.addedDescription.text = ""
 
     def press_cancel(self):
         self.root.ids.popup.dismiss()
         self.clear_fields()
-        self.status_text = ""
+        self.action_label = ""
 
 
 
--- itemlist.py
+++ itemlist.py
@@ -1,1 +1,18 @@
+# from Assignment1 import loading_items
 
+
+class ItemList():
+    def list_items(self):
+        label_display = 'Choose action from the left menu, then select items on the right'
+        self.root.ids.list_items.text = "List Items"
+        self.root.ids.action_label.text = str(label_display)
+        return label_display
+
+    # def load_item(self):
+    #     file = 'items.csv'
+    #     in_file = open(file, 'r', encoding='utf-8')
+    #     items = in_file.readlines()
+    #     in_file.close()
+    #     for line in items:
+    #         section = line.split(',')
+    #         print(section)
