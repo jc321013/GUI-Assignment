d5521f8f34ac4fa331ca1dcb3c94a990f6047955
jc321013 <jared.marcolongo@my.jcu.edu.au>
2016-05-26 22:21:23 +1000

    finishing the hiring items, listing items, and returning items, making their buttons status to normal and down

--- AppA2.py
+++ AppA2.py
@@ -11,8 +11,12 @@ from kivy.properties import StringProperty
 from item import Item
 from itemlist import ItemList
 from Assignment1 import loading_items
-from Assignment1 import listing_items
 from Assignment1 import saving_items
+
+List_Items = 0
+Hire_Items = 1
+Return_Items = 2
+Add_Items = 3
 
 
 class ExperimentHire(App):
@@ -21,81 +25,136 @@ class ExperimentHire(App):
     def __init__(self, **kwargs):
         """Constructs main app"""
         super(ExperimentHire, self).__init__(**kwargs)
-        self.experimentHire = ItemList
-        self.experimentHire = []
 
-    def __getattr__(self, item):
-        self.items = item
-        return
+
 
     def build(self):
         """Builds the Kivy Gui and references to the root Kivy Widget"""
         self.title = "Experiment Hire"
         self.root = Builder.load_file('GUI A2.kv')
+        self.mode = List_Items
         self.create_entry_buttons()
+        labelButton = "Choose Action From The left, then Select Items From the Right"
+        self.action_label = str(labelButton)
+        self.item_listing = []
+        self.list_item = ItemList()
+        item_store = loading_items()
+        self.list_item.save_item(item_store)
         return self.root
 
-    def list_items(self):
+    def button_status(self, status):
+        while status != 'in' or 'out':
+            if status == 'in':
+                pass
+            elif status == 'out':
+                pass
+
+    def list_items(self, items_avaliable):
+        items_avaliable = self.list_item
+
+
+    def listing_items(self):
         label_display = 'Choose action from the left menu, then select items on the right'
-        self.root.ids.list_items.text = "List Items"
+        self.root.ids.listItems.text = "List Items"
         self.root.ids.action_label.text = str(label_display)
+        self.mode = List_Items
+        self.item_listing = []
+        self.root.ids.listItems.state = 'down'
+        self.root.ids.hireItem.state = 'normal'
+        self.root.ids.addButton.state = 'normal'
+        self.root.ids.itemConfirm.state = 'normal'
+        self.root.ids.returnItem.state = 'normal'
         return label_display
 
     def hiring_item(self):
         label_display = 'Select available items to hire'
-        self.root.ids.hire_item.text = 'Hire Items'
+        self.root.ids.hireItem.text = 'Hire Items'
         self.root.ids.action_label.text = str(label_display)
-        for instance in self.root.ids.hire_item.text:
-            temp_button = Button(text=instance)
-            temp_button.bind(on_release=self.hiring_item)
-        for row in self.root.ids.hire_item.children:
-            if row[3] != 'in':
-                instance.state = 'down'
-                return
-            else:
-                if row[3] != 'out':
-                    instance.state = 'normal'
-                    return
-
+        self.mode = Hire_Items
+        self.item_listing = []
+        self.root.ids.listItems.state = 'normal'
+        self.root.ids.hireItem.state = 'down'
+        self.root.ids.addButton.state = 'normal'
+        self.root.ids.itemConfirm.state = 'normal'
+        self.root.ids.returnItem.state = 'normal'
         self.action_label = ""
         return label_display
 
     def confirm_item(self):
-        for item in self.root.ids.hire_item.text:
-            temp_button = Button(text=item)
-            temp_button.bind(on_release=self.hiring_item)
+        if self.mode == Hire_Items:
+            for item in self.item_listing:
+                item.stock = "out"
+                self.set_button_to_normal()
+            self.listing_items()
+        elif self.mode == Return_Items:
+            for item in self.item_listing:
+                item.stock = "out"
+                self.set_button_to_normal()
+            self.listing_items()
+
+        self.root.ids.listItems.state = 'normal'
+        self.root.ids.hireItem.state = 'normal'
+        self.root.ids.addButton.state = 'normal'
+        self.root.ids.itemConfirm.state = 'down'
+        self.root.ids.returnItem.state = 'normal'
+
 
     def return_item(self):
         label_display = 'Select Items To Return'
         self.root.ids.returnItem.text = 'Return Item'
         self.root.ids.action_label.text = str(label_display)
-        for instance in self.root.ids.returnItem.text:
-            temp_button = Button(text=instance)
-            temp_button.bind(on_release=self.return_item)
-        for row in self.root.ids.returnItem.children:
-            if row[3] != 'in':
-                instance.state = 'down'
-                return
-            else:
-                for instance in self.root.ids.entriesBox.children:
-                    instance.state = 'normal'
-                self.status_text = ""
+        self.mode = Return_Items
+        self.item_listing = []
+        self.root.ids.listItems.state = 'normal'
+        self.root.ids.hireItem.state = 'normal'
+        self.root.ids.addButton.state = 'normal'
+        self.root.ids.itemConfirm.state = 'normal'
+        self.root.ids.returnItem.state = 'down'
+        return label_display
+
+    def set_button_to_normal(self):
+        for button in self.root.ids.experiment_hire.children:
+            button.state = 'normal'
+            self.item_listing = []
 
     def create_entry_buttons(self):
         """Constructs the entry buttons and allows the GUI to access them"""
-        for name in self.experimentHire:
-            temp_button = Button(text=name)
+        for item in self.list_item.items:
+            temp_button = Button(text=item.name)
             temp_button.bind(on_release=self.press_entry)
             #  using the add_widget it adds the button to the ""newItems""
             self.root.ids.newItem.add_widget(temp_button)
 
     def press_entry(self, instance):
         """handles the pressing of entry buttons"""
-        name = instance.text
-        self.action_label = "{}, {}, {}".format(name[0], self.experimentHire[name][1], self.experimentHire[name][2])
-        # set button state
-        # print(instance.state)
-        instance.state = 'down'
+        item = self.list_item.display_item_list(instance.text)
+        if self.mode == List_Items:
+            self.action_label = str(item)
+        elif self.mode == Hire_Items:
+            if item.stock == 'in':
+                if item not in self.item_listing:
+                    self.item_listing.append(item)
+                    instance.state = 'down'
+                    total = 0
+                    name = []
+                    for item in self.item_listing:
+                        total += item.price
+                        name.append(item.name)
+                    self.action_label = "Hiring: {} for ${}".format(', '.join(name), total)
+        elif self.mode == Return_Items:
+            if item.stock == 'out':
+                if item not in self.item_listing:
+                    self.item_listing.append(item)
+                    instance.state = 'down'
+                    names = []
+                    for item in self.item_listing:
+                        names.append(item.name)
+                    self.action_label = "Returning: {}".format(', '.join(names))
+
+        elif self.mode == Add_Items:
+            pass
+        self.action_label = "{}, {}, {}".format(item[0], self.experimentHire[item][1], self.experimentHire[item][2])
+
 
     def item_add(self):
         """
@@ -110,38 +169,49 @@ class ExperimentHire(App):
     def press_add(self):
         """Handles the pressing of the add buttons, and the display of the label"""
         self.root.ids.popup_label.text = "Enter details for new Item"
+        self.mode = Add_Items
+        self.item_listing = []
+        self.root.ids.listItems.state = 'normal'
+        self.root.ids.hireItem.state = 'normal'
+        self.root.ids.addButton.state = 'down'
+        self.root.ids.itemConfirm.state = 'normal'
+        self.root.ids.returnItem.state = 'normal'
         # this controls the opening of the popup
         self.root.ids.popup.open()
 
     # Saves the new entry to the front page of the GUI, and controls the pressing of the save button
     def press_save(self, added_name, added_description, added_price):
-        self.experimentHire.append(receive_object(added_name, added_description, added_price))
+        try:
+            self.list_item.add_to_list(added_name, added_description, float(added_price), 'in')
+            # self.experimentHire.append(receive_object(added_name, added_description, added_price))
 
-        label_display = "All fields must be completed"
-        price_label = "Must be a valid number"
-        if added_name == "" or added_description == "" or added_price == "":
-            self.root.ids.popup_label.text = label_display
-            return label_display
+            label_display = "All fields must be completed"
+            price_label = "Must be a valid number"
+            if added_name == "" or added_description == "" or added_price == "":
+                self.root.ids.popup_label.text = label_display
+                return label_display
 
-        # while not added_price < 0:
-        #     try:
-        #         while added_price < 0:
-        #             self.root.ids.popup_label = str(price_label)
-        #             return price_label
-        #     except ValueError:
-        #         self.root.ids.popup_label = str(price_label)
-        #         return price_label
+            # while not added_price < 0:
+            #     try:
+            #         while added_price < 0:
+            #             self.root.ids.popup_label = str(price_label)
+            #             return price_label
+            #     except ValueError:
+            #         self.root.ids.popup_label = str(price_label)
+            #         return price_label
 
-        # self.experimentHire[added_name] = added_description, added_price
-        # Number columns is depended upon the number of entries
-        self.root.ids.newItem.cols = len(self.experimentHire)
-        temp_button = Button(text=added_name)
-        temp_button.bind(on_release=self.press_entry)
-        self.root.ids.newItem.add_widget(temp_button)
-        # dismisses the popup closing it
-        self.root.ids.popup.dismiss()
-        # Clears the fields
-        self.clear_fields()
+            # self.experimentHire[added_name] = added_description, added_price
+            # Number columns is depended upon the number of entries
+            self.root.ids.newItem.cols = len(self.list_item.items)
+            temp_button = Button(text=added_name)
+            temp_button.bind(on_release=self.press_entry)
+            self.root.ids.newItem.add_widget(temp_button)
+            # dismisses the popup closing it
+            self.root.ids.popup.dismiss()
+            # Clears the fields
+            self.clear_fields()
+        except ValueError:
+            self.root.ids.addedPrice.text = ''
 
     def clear_fields(self):
         """ Puts empty string in the text input fields from add entry"""
@@ -155,44 +225,16 @@ class ExperimentHire(App):
         self.clear_fields()
         self.action_label = ""
 
-
-def receive_object(name, description, price):
-
-    new_item = []
-    # name = input('Item name: ')
-    # while name == '':
-    #     print('Item name cannot be blank.')
-    #     name = input('Item name: ')
-    new_item.append(name)
-
-    # description = input('Description: ')
-    # while description == '':
-    #     print('Item description cannot be blank.')
-    #     description = input('Description: ')
-    new_item.append(description)
-
-    # price_check = False
-    # while not price_check:
-    #     try:
-    #         price = float(input('Price per day: '))
-    #         if price < 0:
-    #             print("Price must be >= $0")
-    #         else:
-    #             new_item.append(str(price))
-    #             price_check = True
-    #     except ValueError:
-    #         print("Not a valid number")
-    new_item.append(str(price))
-
-    new_item.append('in')
-    # print("{} ({}), ${} now available for hire.".format(new_item[0], new_item[1], new_item[2]))
-    return new_item
+    def GUI_finish(self):
+        saving_items(self.root.list_item.display_item_as_list())
+        return
 
 
 
 
 
 
-# create and start the App running
 
+
+"""create and start the App running"""
 ExperimentHire().run()
--- itemlist.py
+++ itemlist.py
@@ -3,26 +3,26 @@ import csv
 
 
 class ItemList:
-    def __init__(self, Item, saving_items, loading_items):
-        self.item_list = []
+    def __init__(self):
+        self.items = []
 
     def display_item_list(self, name):
-        for items in self.item_list:
-            if items.name == name:
-                return items
+        for item in self.items:
+            if item.name == name:
+                return item
         return None
 
 
     def save_item(self, items):
         for item in items:
             store_item = Item(item[0], item[1], float(item[2]), item[3].strip())
-            self.item_list.append(store_item)
+            self.items.append(store_item)
 
     def display_item_as_list(self):
         item_store = []
-        for item in self.item_list:
+        for item in self.items:
             item_store.append([item.name, item.description, item.price, item.stock])
             return item_store
 
-    def add_to_list(self, name, description, price, stock):
-        self.item_list.append(Item(name, description, price, stock))
+    def add_to_list(self, added_name, added_description, added_price, added_stock):
+        self.items.append(Item(added_name, added_description, added_price, added_stock))
